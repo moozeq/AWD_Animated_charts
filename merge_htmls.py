8bd9726f0e1604e21d53ac10d4c286118c05b807
Amon <j.niedzw@gmail.com>
2020-05-26 04:28:27 +0200

    [Feature] Added .png ext

--- merge_htmls.py
+++ merge_htmls.py
@@ -7,12 +7,12 @@ def main(args):
     from os.path import isfile, join
     html_files = [f for f in listdir(args.directory) if isfile(join(args.directory, f))]
     html_files = sorted(html_files)
-    html_files = [f'{args.directory}/{file}' for file in html_files if file.endswith('.html') or file.endswith('.gif')]
+    html_files = [f'{args.directory}/{file}' for file in html_files if file.endswith('.html') or file.endswith('.gif') or file.endswith('.png')]
     html = ''
     for filename in html_files:
         if filename.endswith('.html'):
             html += f'<div style="text-align: center;"><p align="center"><iframe src="{filename}" width="80%" height="80%"></iframe></p></div>\n'
-        elif filename.endswith('.gif'):
+        elif filename.endswith('.gif') or filename.endswith('.png'):
             html += f'<div style="text-align: center;"><p align="center"><img src="{filename}" width="90%" height="auto"></img></p></div>\n'
     html += ''
 
